19540f9c64d06cec1a329bd3b46d397d6796f4d8
SinYaYa-SoVa <misbit75@yandex.ru>
2021-05-09 17:57:42 +0300

    Quick update

--- incidents/admin.py
+++ incidents/admin.py
@@ -5,13 +5,9 @@ from django.contrib import admin
 from .models import Incident
 from .models import Employee
 from .models import Department
-from .models import Info_assets
-from .models import Info_objects
 from .models import GRIIB
 
 admin.site.register(Incident)
 admin.site.register(Employee)
 admin.site.register(Department)
-# admin.site.register(Info_assets)
-# admin.site.register(Info_objects)
 admin.site.register(GRIIB)
